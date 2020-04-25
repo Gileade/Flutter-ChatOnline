6bb8fe6439549c360a7d450984ea06f353fb1213
Gileade Trindade <gileade.trindade@gmail.com>
2020-04-25 17:49:55 -0300

    Add Texto no Firebase

--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 
 class TextComposer extends StatefulWidget {
+
+  TextComposer(this.sendMessage);
+  Function (String) sendMessage;
+
   @override
   _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
 
+  final TextEditingController _controller = TextEditingController();
+
   bool _isComposed = false;
+
+  void _reset(){
+    _controller.clear();
+    setState(() {
+      _isComposed = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 55.0),
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
       child: Row(
         children: <Widget>[
           IconButton(
@@ -23,6 +36,7 @@ class _TextComposerState extends State<TextComposer> {
           ),
           Expanded(
             child: TextField(
+              controller: _controller,
               decoration: InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
               onChanged: (text){
                 setState(() {
@@ -30,14 +44,16 @@ class _TextComposerState extends State<TextComposer> {
                 });
               },
               onSubmitted: (text){
-
+                widget.sendMessage(text);
+                _reset();
               },
             ),
           ),
           IconButton(
             icon: Icon(Icons.send),
             onPressed: _isComposed ? (){
-
+              widget.sendMessage(_controller.text);
+              _reset();
             } : null,
           )
         ],
